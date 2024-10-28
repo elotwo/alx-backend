0848fd3ff6559c89f3e780030f9a1e04c864c0f8
elotwo <obiagwu94@gmail.com>
2024-10-28 22:38:18 +0100

    pagnation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
+"""
+this script is about pagination
+"""
+
+
 def index_range(page, page_size):
+    """
+    indext_range takes two arguiment to
+    return start and end of a page
+    """
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
