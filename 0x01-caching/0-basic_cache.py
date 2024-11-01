6b77382aa67b5495a7472d6bf3559f0401cf98f3
elotwo <obiagwu94@gmail.com>
2024-11-02 00:20:20 +0100

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" cache """
+"""cache"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ basic cache """
+    """basic cache"""
     def __init__(self):
-        """ initializing"""
+        """initializing"""
         super().__init__()
 
     def put(self, key, item):
@@ -17,7 +17,7 @@ class BasicCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ na so"""
+        """na so"""
         if key is None or key not in self.cache_data:
             return(None)
         return self.cache_data[key]
