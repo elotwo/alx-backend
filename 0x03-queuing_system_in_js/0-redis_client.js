dac1c6a6aa111e20362129a9ecbe8aa93fba29a4
elotwo <obiagwu94@gmail.com>
2024-12-03 22:50:06 +0100

    update

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+
+const client = createClient ({
+	url: 'redis://localhost:6379',
+});
+client.on('connect', () => {
+	console.log('Redis client connected to the server');
+});
+client.on('error', (err) => {
+	console.log('Redis client not connected to the server: ERROR_MESSAGE', err);
+});
+(async () => {
+	    await client.connect();
+})();
