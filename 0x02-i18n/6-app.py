d5e3ba9fbfc03f75dfdd9e32802b06d169c130a3
elotwo <obiagwu94@gmail.com>
2025-01-21 00:39:32 +0100

    update

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    """Configuration for available languages."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user():
+    """Retrieve user from users dict."""
+    login_as = request.args.get('login_as')
+    if login_as and login_as.isdigit():
+        user_id = int(login_as)
+        return users.get(user_id)
+    return None
+
+@app.before_request
+def before_request():
+    """Set user in global context before each request."""
+    g.user = get_user()
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    # 1. Locale from URL parameters
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    # 2. Locale from user settings
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    # 3. Locale from request headers
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """Render the homepage."""
+    return render_template('6-index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
