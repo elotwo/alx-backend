f192c3c452f0ad1cd60a68073cb9d01faddbe659
elotwo <obiagwu94@gmail.com>
2025-01-21 00:53:24 +0100

    update

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,21 +2,25 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """Configuration class for Flask app."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)  # Instantiate Babel with the app
 
+
 @app.route('/')
 def index():
     """Render the homepage with a welcome message."""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -4,11 +4,13 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """Configuration for available languages."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 babel = Babel(app)
@@ -20,6 +22,7 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 def get_user():
     """Retrieve user from users dict."""
     login_as = request.args.get('login_as')
@@ -28,10 +31,12 @@ def get_user():
         return users.get(user_id)
     return None
 
+
 @app.before_request
 def before_request():
     """Set user in global context before each request."""
     g.user = get_user()
+
 
 @babel.localeselector
 def get_locale():
@@ -46,10 +51,12 @@ def get_locale():
     # 3. Locale from request headers
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Render the homepage."""
     return render_template('6-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
